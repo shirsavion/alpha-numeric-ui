87632ea935fffa29c190427f97ffc79a987383dd
Fritzzzz <friedmanliran@gmail.com>
2022-09-09 15:52:33 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import LOGO from './assets/logo_full.png'
+import GO from './assets/go.png'
+import styled from 'styled-components'
+import {useState} from 'react'
+import Colors from './Colors'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <Container>
+            <Logo src={LOGO} />
+            <GoButton>
+                <Go src={GO} />
+            </GoButton>
+        </Container>
+    )
 }
 
-export default App;
+export default App
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    background-color: ${Colors.BLUE};
+    width: 100vw;
+    height: 100vh;
+    align-items: center;
+    justify-content: center;
+`
+const Logo = styled.img`
+    height: 500px;
+`
+const Go = styled.img`
+    height: 100px;
+    width: 200px;
+`
+const GoButton = styled.button`
+    background: transparent;
+    border: 0px;
+`
---
+++ src/Colors.js
@@ -0,0 +1,9 @@
+export default {
+    BLUE: '#7399FB',
+    LIGHT_GREY: '#d0d1d3',
+    WHITE_SMOKE: '#e6e6e6',
+    GREEN: '#00ff8b',
+    TRANSPARENT_GREY: '#97919799',
+    LIGHT_PINK_NEW: '#EF1C4A40',
+    PINK_NEW: '#FFC0CE',
+}
