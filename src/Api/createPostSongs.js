a8b5422b863c5bd5777d7d42bad7308236f78128
Fritzzzz <friedmanliran@gmail.com>
2022-09-10 17:57:15 +0300

    refactor

--- src/Api/createPostSongs.js
+++ src/Api/createPostSongs.js
@@ -1,19 +1,20 @@
 import {apiRoutes} from '../routes'
 import axios from 'axios'
 
-export default function postSongs(urls, queryOptions) {
+export default postSongs = (urls, queryOptions) => {
     axios({
         method: 'post',
         url: apiRoutes.dummyPost,
         headers: {},
         data: {
             urls: urls,
-            advanced: queryOptions
-        }
-    }).then((response) => {
-        console.log('[TAMIR] Im ok!' + response.data)
-        return response.data
+            advanced: queryOptions,
+        },
     })
+        .then((response) => {
+            console.log('[TAMIR] Im ok!' + response.data)
+            return response.data
+        })
         .catch((e) => {
             console.error('[TAMIR] Im a error!' + e.message || e)
             return -1
--- src/Api/pollUpdates.js
+++ src/Api/pollUpdates.js
@@ -1,19 +1,22 @@
 import {apiRoutes} from '../routes'
-import axios from "axios"
-import React from 'react'
+import axios from 'axios'
 
-
-export default function pollUpdates(songId) {
-    axios.get(apiRoutes.pollUpdates + songId ).then((response) => { //Add songId to query
-        if (response) {
-            if (response.data?.isReady) {
-                const song = response.data?.file64
-                var snd = new Audio("data:audio/wav;base64," + song);
-                snd.play();
+export default pollUpdates = (requestId) => {
+    axios
+        .get(apiRoutes.pollUpdates + requestId)
+        .then((response) => {
+            if (response) {
+                const status = response?.data?.state
+                let audio = null
+                if (response.data?.isReady) {
+                    const song = response.data?.file64
+                    audio = new Audio('data:audio/wav;base64,' + song)
+                    // snd.play()
+                }
+                return {status, audio}
             }
-            return (response.data.state)
-        }
-    }).catch((e) => {
-        console.error("[TAMIR] Im a error!" + e.message || e)
-    })
+        })
+        .catch((e) => {
+            console.error('[TAMIR] Im a error!' + e?.message || e)
+        })
 }
