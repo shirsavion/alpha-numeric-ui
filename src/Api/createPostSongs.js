4ef9871f0a2066bb81a9e43e8a2ca20edd1f3a57
Fritzzzz <friedmanliran@gmail.com>
2022-09-10 18:33:01 +0300

    refactor

--- src/Api/createPostSongs.js
+++ src/Api/createPostSongs.js
@@ -2,35 +2,18 @@ import {apiRoutes} from './routes'
 import axios from 'axios'
 
 const postSongs = (urls, queryOptions) => {
-    axios.post(apiRoutes.dummyPost, {
-        urls: urls,
-        advanced: queryOptions}
-    )
+    axios
+        .post(apiRoutes.dummyPost, {
+            urls: urls,
+            advanced: queryOptions,
+        })
         .then((response) => {
             const {requestId, msg: status} = response?.data
-            return {status , requestId}
-    }
-    ).catch((e) => {
-        console.error(e?.message || e)
-    })
+            return {status, requestId}
+        })
+        .catch((e) => {
+            console.error(e?.message || e)
+        })
 }
 
-    //     axios({
-//         method: 'post',
-//         url: apiRoutes.dummyPost,
-//         headers: {},
-//         data: {
-//             urls: urls,
-//             advanced: queryOptions,
-//         },
-//     })
-//         .then((response) => {
-//             console.log('[TAMIR] Im ok!' + response.data)
-//             return response.data
-//         })
-//         .catch((e) => {
-//             console.error('[TAMIR] Im a error!' + e.message || e)
-//             return -1
-//         })
-// }
 export default postSongs
